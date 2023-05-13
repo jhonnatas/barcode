620d6688deabc7a3b2349ad4ae316eb301fe421d
jhonnatas <jhonnatas.aires@gmail.com>
2023-05-13 16:04:33 -0300

    recator: refactor GenBarcode class

--- app/service/GenBarcode.rb
+++ app/service/GenBarcode.rb
@@ -1,5 +1,5 @@
 class GenBarcode < ApplicationService
-	attr_reader :item
+attr_reader :item
 	
   def initialize(item)
     @item = item
@@ -9,11 +9,11 @@ class GenBarcode < ApplicationService
 		# build_label
 		label = build_label
 
-		image1 = get_image_1
-		image2 = get_image_2
+		image1 = get_image('ifce.png', 82, 40, 305, 65)
+		image2 = get_image('ifce.png', 525, 40, 305, 65)
 		
-		barcode1 = left_barcode(@item.numero)
-		barcode2 = right_barcode(@item.numero)	
+		barcode1 = draw_barcode(@item.numero, 85, 110, 70)
+		barcode2 = draw_barcode(@item.numero, 525, 110, 70)	
 
 		# Set image background to white
 		src = image1.source
@@ -34,54 +34,31 @@ class GenBarcode < ApplicationService
 
 	def send_to_printer(label, ip)
 		print_job = Zebra::PrintJob.new 'zebra'
-		print_job.print label, ip, print_service: 'rlpr' # pra isso funcionar, tem que configurar o lpd no windows 
+		print_job.print label, ip, print_service: 'rlpr' # To get this working setup lpd for windows.
 	end
 
 	def build_label
 		Zebra::Zpl::Label.new(
-			width:        800, #width of the barcode image on the label
+			width:        800, # total width of the two barcodes image side by side on the label
 			length:       30,
 			print_speed:  3
 		)			
 	end
 
-	def get_image_1
-		#image1 = Zebra::Zpl::Image.new(
+	def get_image(file_name, pos_x, pos_y, width, height)
 		Zebra::Zpl::Image.new(
-			path: "#{Rails.root}/public/ifce.png",
-			position: [82, 40],
-			width: 305,
-			height: 65
-		)
-	end
-	
-	def get_image_2
-		#image2 = Zebra::Zpl::Image.new(
-		Zebra::Zpl::Image.new(
-			path: "#{Rails.root}/public/ifce.png",
-			position: [525, 40],
-			width: 305,
-			height: 65
+			path: "#{Rails.root}/public/#{ file_name }",
+			position: [pos_x, pos_y],
+			width: width,
+			height: height
 		)
 	end
 
-	def left_barcode(tipping_number)
+	def draw_barcode(tipping_number, pos_x, pos_y, height)
 		Zebra::Zpl::Barcode.new(
 			data:                       tipping_number,
-			position:                   [85, 110], # x, y
-			height:                     70,
-			print_human_readable_code:  true,
-			narrow_bar_width:           2,
-			wide_bar_width:             8,
-			type:                       Zebra::Zpl::BarcodeType::CODE_128_AUTO
-		)
-	end
-
-	def right_barcode(tipping_number)
-		Zebra::Zpl::Barcode.new(
-			data:                       tipping_number,
-			position:                   [525, 110],
-			height:                     70,
+			position:                   [pos_x, pos_y], # x, y
+			height:                     height,
 			print_human_readable_code:  true,
 			narrow_bar_width:           2,
 			wide_bar_width:             8,
